5bd1c70a10239314bbdbfa276b65700f831c0077
ISAWASHUN <syun20000616@icloud.com>
2024-01-16 11:12:19 +0900

    Transactionの導入

--- app/models/ranks_updater.rb
+++ app/models/ranks_updater.rb
@@ -1,10 +1,12 @@
 class RanksUpdater
   def update_all
+    Rank.transaction do
     # 現在のランキング情報をリセット
-    Rank.delete_all
+      Rank.delete_all
 
     # ユーザーごとのスコア合計を降順に並べ替え、そこからランキング情報を再作成する
-    create_ranks
+      create_ranks
+    end
   end
 
   private
